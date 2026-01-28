ed3bacb855955becd6192c6d3775b8714489f4e7
Nitya-Prakash <nityaprakashsahoo6776@gmail.com>
2026-01-28 22:43:28 +0530

    Connect to mongodb

---
+++ Day-6-Database/server.js
@@ -0,0 +1,18 @@
+const app = require("./src/app.js");
+const mongoose = require("mongoose");
+
+function connectToDb() {
+  mongoose
+    .connect(
+      "mongodb+srv://prakashnitya07_db_user:F4k2qkrZIL03JhS4@cluster0.1tuewkm.mongodb.net/Day-6",
+    )
+    .then(() => {
+      console.log("Connected to Database!");
+    });
+}
+
+connectToDb();
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
